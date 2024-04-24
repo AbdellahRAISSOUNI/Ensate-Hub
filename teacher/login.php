3efd0ac6073d11450fe5e6127130324910b89b98
AbdellahRAISSOUNI <abdellahraissouni@gmail.com>
2024-04-24 11:43:36 +0100

    fix

--- teacher/login.php
+++ teacher/login.php
@@ -108,7 +108,7 @@ echo "<script>alert('Invalid Details');</script>";
                 <div class="col-lg-6 col-lg-offset-3">
                     <div class="login-content">
                         <div class="login-logo">
-                            <a href="../index.php"><span>Ensaté-Hub - Projet Web réalisé par des Ensatiens</span></a>
+                            <a href="../index.php"><span>Ensaté-Hub</span></a>
                         </div>
                         <div class="login-form">
                             <h4>Professeur : Connexion</h4>
