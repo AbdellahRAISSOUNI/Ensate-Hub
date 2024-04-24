e5ae9071b5bf9d51fdd225eb5d4a88cc102b6e36
AbdellahRAISSOUNI <abdellahraissouni@gmail.com>
2024-04-24 20:30:31 +0100

    images

--- user/includes/dbconnection.php
+++ user/includes/dbconnection.php
@@ -2,7 +2,7 @@
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','Ossamasm123');
+define('DB_PASS','1003');
 define('DB_NAME','ocasdb');
 
 // Establish database connection.
