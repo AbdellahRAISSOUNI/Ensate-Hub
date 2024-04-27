eb14fcacdfc6147748a29eedde0e2dc0c04f2f85
AbdellahRAISSOUNI <abdellahraissouni@gmail.com>
2024-04-27 12:23:44 +0100

    login fixed

--- teacher/includes/dbconnection.php
+++ teacher/includes/dbconnection.php
@@ -1,17 +1,13 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-<<<<<<< HEAD
-define('DB_PASS','1234'); // Updated password
-=======
-define('DB_PASS','Ossamasm123'); // Updated password
->>>>>>> 0b415d43d3a1cf945e92b7daecfc46d302795232
-define('DB_NAME','ocasdb');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '1003'); // Updated password
+define('DB_NAME', 'ocasdb');
 
 // Establish database connection.
 try {
-    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
