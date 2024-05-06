b83aa58858ce930d264ed5b8af813a27cc43d3ac
AbdellahRAISSOUNI <abdellahraissouni@gmail.com>
2024-05-06 13:21:27 +0100

    visibilité

--- teacher/includes/dbconnection.php
+++ teacher/includes/dbconnection.php
@@ -1,11 +1,11 @@
 <?php 
-// DB credentials.
+// connection avec la base de donnés
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'Ossamasm123'); // Updated password
-define('DB_NAME', 'ocasdb');
+define('DB_PASS', '1003');
+define('DB_NAME', 'web-Ensa');
 
-// Establish database connection.
+// la connection:
 try {
     $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 } catch (PDOException $e) {
